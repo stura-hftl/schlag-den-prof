9cbb297be908d1fc8f3470fa6ffa4373058f05d2
Sven Walter <sven@wltr.eu>
2013-12-06 19:06:20 +0100

    Mappick game: improved viewport

---
+++ src/main/resources/schlagdenprof/js/common/map.js
@@ -0,0 +1,68 @@
+define(function(require){
+
+	var pseudoClass = function($canvas, bounds){
+		var self = {};
+		var our = {};
+
+		// --- STATIC VARS ---
+		our.styles = {};
+		our.styles.running = [
+			{ featureType: "poi",    stylers: [ { visibility: "off" } ] },
+			{ elementType: "labels", stylers: [ { visibility: "off" } ] },
+		];
+		our.styles.finish = [];
+
+		// --- PSEUDO CONSTRUCTOR ---
+		// called at bottom
+		var init = function() {
+			our.$canvas = $canvas;
+
+			our.bounds = new google.maps.LatLngBounds();
+			our.bounds.extend(new google.maps.LatLng(bounds[0][0], bounds[0][1]));
+			our.bounds.extend(new google.maps.LatLng(bounds[1][0], bounds[1][1]));
+
+			our.map = new google.maps.Map(our.$canvas[0], {
+				zoom: 20,
+				center : our.bounds.getCenter(),
+				mapTyoeId: google.maps.MapTypeId.ROADMAP,
+				disableDefaultUI: true,
+				draggable: false,
+				styles: our.styles.running,
+
+			});
+
+
+
+		};
+
+
+		// --- PUBLIC FUNCTIONS ---
+		self.resize = function(){
+			// set bounds
+			our.map.fitBounds(our.bounds);
+
+			// WORKAROUND: Make sure google checks div resize.
+			//             This might fail ...
+			google.maps.event.trigger(our.map, "resize");
+			
+
+		}
+
+		self.setMarker = function(key, lat, lng){
+			var latlng = new google.maps.LatLng(lat, lng);
+			var marker = new google.maps.Marker({
+				position: latlng,
+				draggable: true
+			});
+			marker.setMap(our.map);
+
+		}
+		
+		init();
+		return self;
+
+	};
+
+
+	return pseudoClass;
+});
--- src/main/resources/schlagdenprof/js/games/mappick.js
+++ src/main/resources/schlagdenprof/js/games/mappick.js
@@ -3,74 +3,24 @@ define(function(require){
 	var our = {};
 
 	// --- IMPORTS ---
+	var Map = require("common/map");
 	var StacheBeamer = require("stache!html/game-mappick-beamer");
 
-	// --- PRIVATE VARS ---
-	our.center = {
-		latitude: 51.320833,
-		longitude : 12.373611
-	};
-	our.center.latlng = new google.maps.LatLng(our.center.latitude,
-					 					our.center.longitude);
-
-	// --- PRIVATE FUNCTIONS ---
-	var fixMapSize = function() {
-		// WORKAROUND: Make sure google checks div resize.
-		//             This might fail ...
-		google.maps.event.trigger(our.map, "resize");
-		our.map.setCenter(our.center.latlng);
-
-	};
-
-	
 	// --- STATIC BLOCK ---
 
 	// --- PUBLIC FUNCTIONS ---
 	self.drawBeamer = function(args, state, data){
-		if(!our.$beamer) {
-			var styleArray = [
-				{
-					featureType: "poi",
-					stylers: [
-						{ visibility: "off" }
-					]
-				},
-				{
-					elementType: "labels",
-					stylers: [
-						{ visibility: "off" }
-					]
-				}
-			];
 
+		var args = args[0];
+		var $beamer = $(StacheBeamer());
+		var $canvas = $beamer.filter("#mappick_canvas");
 
-			our.$beamer = $(StacheBeamer());
-			our.$canvas = our.$beamer.filter("#mappick_canvas");
-			our.map = new google.maps.Map(our.$canvas[0], {
-					zoom: 15,
-					center: our.center.latlng,
-					mapTypeId: google.maps.MapTypeId.ROADMAP,
-					disableDefaultUI: true,
-					draggable: false,
-					styles: styleArray
-			});
+		var map = Map($canvas, args.bounds);
+		map.setMarker("center", args.target[0], args.target[1]);
 
-			var myMarker = new google.maps.Marker({
-					position: our.center.latlng,
-					draggable: true
-			});
+		window.setTimeout(map.resize, 200);
 
-			myMarker.setMap(our.map);
-
-		}
-		window.setTimeout(fixMapSize, 100);
-		window.setTimeout(fixMapSize, 200);
-		window.setTimeout(fixMapSize, 300);
-		window.setTimeout(fixMapSize, 500);
-		window.setTimeout(fixMapSize, 800);
-		window.setTimeout(fixMapSize, 1300);
-
-		return our.$beamer;
+		return $beamer;
 
 	};
 
