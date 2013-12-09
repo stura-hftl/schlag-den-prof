99e02b56920f10a6e12208719e9256b152cc4425
Sven Walter <sven@wltr.eu>
2013-12-09 23:04:23 +0100

    Created example config. Thus, I broke everything -- haha.

--- devserver.sh
+++ devserver.sh
@@ -2,4 +2,4 @@
 
 #mvn -q clean package
 
-java -cp src/main/resources/:target/schlagdenprof-2.0.0-SNAPSHOT.jar:src/main/resources/ eu.wltr.schlagdenprof.SchlagDenProf data_2013
+java -cp src/main/resources/:target/schlagdenprof-2.0.0-SNAPSHOT.jar:src/main/resources/ eu.wltr.schlagdenprof.SchlagDenProf example_data
