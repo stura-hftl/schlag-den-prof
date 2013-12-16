4b9416f7d99720e01fc6d329677d4049f6638eae
Sven Walter <sven@wltr.eu>
2013-12-16 15:20:30 +0100

    Added countdown game.

--- src/main/resources/schlagdenprof/js/games/countdown.js
+++ src/main/resources/schlagdenprof/js/games/countdown.js
@@ -2,6 +2,7 @@ define(function(require){
 
 	var self = {};
 	var our = {};
+	our.interval;
 
 	//var StacheBeamer = require("stache!html/game-text-beamer");
 
@@ -9,7 +10,30 @@ define(function(require){
 	self.name = "Countdown"
 
 	// --- PUBLIC FUNCTIONS ---
-	self.drawBeamer = function(args, state, data){
+	self.drawBeamer = function(gc){
+		duration = gc.getArg(0);
+
+		var $el = $("<div>");
+		$el.addClass("layer layer-c layer-bigtext");
+
+		window.clearInterval(our.interval);
+
+		$el.text(duration);
+
+		our.interval = window.setInterval(function(){
+			duration = duration - 0.1;
+
+			$el.text(duration.toFixed(1));
+
+			if(duration <= 0)
+			{
+				$el.text("0.0");
+				window.clearInterval(our.interval);
+			}
+
+		}, 100);
+
+		return $el;
 	};
 
 	self.drawControl = function(gc) {
